0913c01514c72b00215108d3ad94f67b0d346891
Cami <Cami@LAPTOP-GRDN8A38>
2024-10-27 23:08:36 -0300

    CollectionInmobiliaria precargarInmobiliarias Tejeda Camila

---
+++ src/ar/edu/unju/escmi/tp4/collections/CollectionInmobiliaria.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.escmi.tp4.collections;
+
+import java.util.ArrayList;
+
+import ar.edu.unju.escmi.tp4.dominio.Inmobiliaria;
+
+public class CollectionInmobiliaria {
+    public static ArrayList<Inmobiliaria> inmobiliarias = new ArrayList<>();
+
+    public static void precargarInmobiliarias() {
+        inmobiliarias.add(new Inmobiliaria("Inmobiliaria Casa Bonita", "123456789", "Av. Siempre Viva 123"));
+        inmobiliarias.add(new Inmobiliaria("Inmobiliaria Hogar Dulce Hogar", "987654321", "Calle Falsa 456"));
+        inmobiliarias.add(new Inmobiliaria("Inmobiliaria Espacio Ideal", "555666777", "Paseo de la Reforma 789"));
+        inmobiliarias.add(new Inmobiliaria("Inmobiliaria Tu Nuevo Hogar", "444333222", "Av. Libertador 101"));
+        inmobiliarias.add(new Inmobiliaria("Inmobiliaria Sueños Realizados", "111222333", "Calle del Sol 202"));
+    }
+}
