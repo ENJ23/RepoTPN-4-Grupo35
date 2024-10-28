2dc47552c03f69f64138b772dbf3219c6b53e2b4
SERGIO <PC@SERGIO>
2024-10-27 22:36:53 -0300

    Clase Contrato Sergio Morales

--- src/ar/edu/unju/escmi/tp4/dominio/Contrato.java
+++ src/ar/edu/unju/escmi/tp4/dominio/Contrato.java
@@ -1,5 +1,47 @@
 package ar.edu.unju.escmi.tp4.dominio;
 
-public class Contrato {
+import java.time.LocalDate;
 
+public abstract class Contrato {
+	protected Cliente cliente;
+    protected LocalDate fechaContrato; 
+    protected Inmobiliaria inmobiliaria;
+
+
+
+    public Contrato(Cliente cliente, LocalDate fechaContrato, Inmobiliaria inmobiliaria) {
+		this.cliente = cliente;
+		this.fechaContrato = fechaContrato;
+		this.inmobiliaria = inmobiliaria;
+	}
+
+	public abstract void mostrarDatos();
+    
+    public abstract double montoTotal();
+
+	public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getFechaContrato() {
+        return fechaContrato;
+    }
+
+    public void setFechaContrato(LocalDate fechaContrato) {
+        this.fechaContrato = fechaContrato;
+    }
+
+	public Inmobiliaria getInmobiliaria() {
+		return inmobiliaria;
+	}
+
+	public void setInmobiliaria(Inmobiliaria inmobiliaria) {
+		this.inmobiliaria = inmobiliaria;
+	}
+    
+    
 }
